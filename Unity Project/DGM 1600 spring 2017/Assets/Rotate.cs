e036fdecb45a8766c25494b28a7799986bc1ae82
Unknown <ryan15215@gmail.com>
2017-01-26 19:57:40 -0700

    Unity Practice with rotation

---
+++ Unity Project/DGM 1600 spring 2017/Assets/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+	public int number = 15;
+	public int rotSpeed = 5;
+	
+	// Use this for initialization
+	void Start () {
+		Debug.Log(number);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Rotate(0,3,0);
+		transform.Rotate(Vector3.up,rotSpeed*Time.deltaTime);
+	}
+}
