3eba8977635810329ad01a961bcd5ece59c292d2
Unknown <ryan15215@gmail.com>
2017-02-09 19:58:22 -0700

    Unity Demo -- Variables and Operators

---
+++ Unity Project/DGM 1600 spring 2017/Assets/Move/Assets/Move.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public int moveSpeed = 10;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*Transform tran = transform;
+        transform.position = new Vector3(0,0,moveSpeed);*/
+
+		transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+	}
+}
---
+++ Unity Project/DGM 1600 spring 2017/Assets/Move/Assets/Operators.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Operators : MonoBehaviour {
+	public int One = 5;
+	public int Two = 2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Arithmeric Operators can be used on both Literal and Variable numbers.
+
+	// Addition -- Adds two values together
+		Debug.Log(One + Two);
+		Debug.Log(One + One);
+		Debug.Log(Two+ Two);
+	// Subtraction -- Subtracts a value from another
+		Debug.Log(One - Two);
+		Debug.Log(One - One);
+		Debug.Log(Two - Two);
+	//Multiplication -- Multiplies two values together
+		Debug.Log(One * Two);
+		Debug.Log(One * One);
+		Debug.Log(Two * Two);
+	//Division -- Divides one value by another
+		Debug.Log(One / Two);
+		Debug.Log(One / One);
+		Debug.Log(Two / Two);
+	// Modulus -- Remaining value after division 
+		Debug.Log(One % Two);
+	//Increment -- Increases a value by 1
+		Debug.Log(++Two);
+	//Decrement -- Decreases a value by 1
+		Debug.Log(--Two);
+	}
+}
