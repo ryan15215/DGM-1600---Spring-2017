a96c9959b49be5b7d2677f217bf9828ccb762e3a
Unknown <ryan15215@gmail.com>
2017-03-09 19:44:59 -0700

    Unity UI Demo and Practice

---
+++ Unity Project/Project/New Project/Assets/levelLoad.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelLoad : MonoBehaviour {
+
+
+	public void LoadLevel(string level){
+
+		SceneManager.LoadScene(level);
+	}
+}
